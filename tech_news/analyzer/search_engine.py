1c07004ac4dda0e2584b6eb4f795b82c2cf105e1
Tiago <sathler@gmail.com>
2022-07-20 21:55:46 -0300

    :sparkles: feat[search_engine.py]: implementa a função 'search_by_category(category)'

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -91,5 +91,21 @@ def search_by_tag(tag: str) -> list:
 
 
 # Requisito 9
-def search_by_category(category):
-    """Seu código deve vir aqui"""
+def search_by_category(category: str) -> list:
+    """Faz uma busca no banco de dados de notícias por 'category'
+    e retorna uma lista de tuplas contendo o 'título' e 'url' da(s) notícia(s)
+
+    Parâmetros:
+    -----------
+    category : str
+        String com o nome da categoria
+
+    Retorno:
+    --------
+    news_list : list[tuple]
+        Uma lista com as tuplas de notícias encontradas
+    """
+    regex_expr = re.compile(category, flags=re.IGNORECASE)
+    news_list = search_news({"category": {"$regex": regex_expr}})
+    news_list = tuple_list_converter(news_list)
+    return news_list
